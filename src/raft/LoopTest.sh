40ece890a5fd203a1104dec987a9549b5844a2fd
pojiang20 <1021465760@qq.com>
2023-02-25 12:03:28 +0800

    chore: 完善测试脚本

--- src/raft/LoopTest.sh
+++ src/raft/LoopTest.sh
@@ -1,4 +1,5 @@
-TestType=3
+TestType=4
+TestRound=100
 #1 background 2A
 if [ $TestType = 1 ]
 then
@@ -6,29 +7,68 @@ then
 fi
 
 #2 Test2A
+echo "start $(date +%Y-%m-%d" "%H:%M:%S)"
 if [ $TestType = 2 ]
 then
   Cnt=0
-  for i in {1..100}
+  for i in $(seq 1 $TestRound)
   do
-    res=$(go test -run -2A | grep ok | wc -l)
+    go test -run 2A > log
+    res=$(cat log | grep PASS | wc -l)
     if ((res==1));then
       ((Cnt=Cnt+1))
+    else
+      mv log "2Aerr_${Cnt}"
     fi
   done
-  echo $Cnt
+  echo "Passing rate: $Cnt/$TestRound"
+  echo "Finish $(date +%Y-%m-%d" "%H:%M:%S)"
 fi
 
-#3 Test2B
+#3 background 2B
 if [ $TestType = 3 ]
 then
+  go test -run 2B > log &
+fi
+
+#4 Test2B
+if [ $TestType = 4 ]
+then
   Cnt=0
-  for i in {1..10000}
+  for i in $(seq 1 $TestRound)
   do
-    res=$(go test -run -2B | grep ok | wc -l)
+    go test -run 2B > log
+    res=$(cat log | grep PASS | wc -l)
     if ((res==1));then
       ((Cnt=Cnt+1))
+    else
+      mv log "2Berr_${Cnt}"
     fi
   done
-  echo $Cnt
+  echo "Passing rate: $Cnt/$TestRound"
+  echo "Finish $(date +%Y-%m-%d" "%H:%M:%S)"
+fi
+
+#5 background 2C
+if [ $TestType = 5 ]
+then
+  go test -run 2C > log &
+fi
+
+#6 Test2C
+if [ $TestType = 6 ]
+then
+  Cnt=0
+  for i in $(seq 1 $TestRound)
+    do
+      go test -run 2C > log
+      res=$(cat log | grep PASS | wc -l)
+      if ((res==1));then
+        ((Cnt=Cnt+1))
+      else
+        mv log "2Cerr_${Cnt}"
+      fi
+    done
+    echo "Passing rate: $Cnt/$TestRound"
+    echo "Finish $(date +%Y-%m-%d" "%H:%M:%S)"
 fi
